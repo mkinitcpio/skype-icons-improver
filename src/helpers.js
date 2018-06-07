4b62d7e7d6017a4ccad1641492d2c49ad1ac7b3d
mkinitcpio <aliaksandr.kazhamiaka@gmail.com>
2018-06-07 19:54:28 +0300

    Release: v1

--- src/helpers.js
+++ src/helpers.js
@@ -1,15 +1,35 @@
-const { spawn } = require('child_process');
+const exec = require('child_process').exec;
+const paths = require('./paths.json');
 
-const installDependencies = () => new Promise((resolve, reject) => {
-    const npm = spawn('npm', ['i']);
-    npm.on('error', reject);
-    npm.on('close', resolve);
-});
+const installDependencies = () => new Promise((resolve, reject) =>
+    exec('npm i', error => error ? reject(error) : resolve())
+);
 
-const unpackSkypeApplication = () => new Promise((resolve, reject) => {
-    const npm = spawn('npm', ['run', 'extract']);
-    npm.stdout.on('error', reject);
-    npm.stdout.on('close', resolve);
-});
+const unpackSkypeApplication = () => new Promise((resolve, reject) =>
+    exec('npm run extract', error => error ? reject(error) : resolve())
+);
 
-module.exports = { installDependencies, unpackSkypeApplication };
+const copyIcons = () => new Promise((resolve, reject) =>
+    exec('yes 2>/dev/null | cp ./src/resources/tray-icons/* ./tmp/extractedCode/images/tray/linux/', error => error ? reject(error) : resolve())
+);
+
+const packSkypeApplication = () => new Promise((resolve, reject) =>
+    exec('npm run pack', error => error ? reject(error) : resolve())
+);
+
+const copyPackedApplication = () => new Promise((resolve, reject) =>
+    exec(`sudo cp ./tmp/packedApp/app.asar ${paths.skype}`, error => error ? reject(error) : resolve())
+);
+
+const clearTempFolders = () => new Promise((resolve, reject) =>
+    exec('rm -rf ./tmp ./node_modules', error => error ? reject(error) : resolve())
+);
+
+module.exports = {
+    installDependencies,
+    unpackSkypeApplication,
+    packSkypeApplication,
+    copyPackedApplication,
+    clearTempFolders,
+    copyIcons
+};
--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ const paths = require('./paths.json');
 
 const {
     unpackSkypeApplication,
-    installDependencies
+    packSkypeApplication,
+    installDependencies,
+    copyPackedApplication,
+    clearTempFolders,
+    copyIcons
 } = require('./helpers');
 
 const main = async () => {
@@ -11,16 +15,36 @@ const main = async () => {
     const pathToSkype = paths.skype;
 
     if (isExist(pathToSkype)) {
+        try {
+            console.log('Installing dependencies...');
+            await installDependencies();
+            console.log('Installation is complete!\n----');
 
-        console.log('Installing dependencies...');
-        await installDependencies();
-        console.log('The installation is complete!\n----');
+            console.log('Unpacking skype packages...');
+            await unpackSkypeApplication();
+            console.log('Unpacking is complete!\n----');
 
-        console.log('Unpacking skype packages...');
-        await unpackSkypeApplication();
-        console.log('Unpacking is complete!');
+            console.log('Copying skype tray icons...');
+            await copyIcons();
+            console.log('Copying is complete!\n----');
+
+            console.log('Packing skype packages...');
+            await packSkypeApplication();
+            console.log('Packing is complete!\n----');
+
+            console.log('Copying packed application...');
+            await copyPackedApplication();
+            console.log('Copying is complete!\n----');
+
+            console.log('Clearing temp folders...');
+            await clearTempFolders();
+            console.log('Clearing is complete!');
+        } catch (error) {
+            console.log(error);
+        }
+
     } else {
-        throw new Error("'Skype for linux' applicaton does not exist");
+        throw new Error("'Skype for linux' does not exist.");
     }
 }
 
